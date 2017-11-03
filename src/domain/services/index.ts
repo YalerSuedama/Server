7967f86a4d6abdc46682c679fd06c9591452b297
Unknown <fabio.cesso@gmail.com>
2017-11-03 11:02:22 -0200

    /ESP0001.1_CreateProjectStructure: Auto stash before merge of "feature/ESP0001.1_CreateProjectStructure" and "origin/feature/ESP0001.1_CreateProjectStructure"
    fix(domain): fixing reference

--- src/domain/services/index.ts
+++ src/domain/services/index.ts
@@ -1,5 +1,5 @@
 export * from "./accountPercentageLiquidityService";
-export * from "./fromConfigAmadeusService";
+export * from "./FromConfigAmadeusService";
 export * from "./constantFeeService";
 export * from "./orderServiceImpl";
 export * from "./reserveManagerTraderService";
