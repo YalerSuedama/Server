d72e002fe33de7ed91d23feab55374c294529a5f
Eduardo Lima <edulima@gmail.com>
2017-11-14 00:18:00 -0200

    test(TokenPairs): token_pairs route should be unit tested

---
+++ src/domain/services/tokensWithLiquidityTokenPairsService.spec.ts
@@ -0,0 +1,6 @@
+describe("TokensWithLiquidityTokenPairsService", () => {
+    it.skip("when tokenA informed, returns pairs with tokenA as left-hand side or right-hand side of pair.");
+    it.skip("when tokenB informed, returns pairs with tokenB as left-hand side or right-hand side of pair.");
+    it.skip("when tokenA and tokenB informed, returns pairs with at least one of the tokens and the other token of the pair is tokenC.");
+    it.skip("when tokenA and tokenB informed, returns pairs with tokenA and tokenB as one pair (in either side of the pair).");
+});
--- src/server/controllers/tokenPairsController.spec.ts
+++ src/server/controllers/tokenPairsController.spec.ts
@@ -2,17 +2,48 @@ import { expect, use } from "chai";
 import "reflect-metadata";
 import * as sinon from "sinon";
 import * as sinonChai from "sinon-chai";
+import { TokenPairsService, TYPES } from "../../app";
+import { TokensWithLiquidityTokenPairsService } from "../../domain";
+import { iocContainer } from "../middleware/iocContainer";
+import { TokenPairsController } from "./tokenPairsController";
 
 use(sinonChai);
 
 describe("TokenPairsController", () => {
-    it.skip("maps to '/api/v0/token_pairs' route.");
     describe(".listPairs", () => {
-        it.skip("calls GET method of base route.");
-        it.skip("expects two parameters: tokenA and tokenB.");
-        it.skip("when tokenA informed, returns pairs with tokenA as left-hand side or right-hand side of pair.");
-        it.skip("when tokenB informed, returns pairs with tokenB as left-hand side or right-hand side of pair.");
-        it.skip("when tokenA and tokenB informed, returns pairs with at least one of the tokens and the other token of the pair is tokenC.");
-        it.skip("when tokenA and tokenB informed, returns pairs with tokenA and tokenB as one pair (in either side of the pair).");
+        let stub: sinon.SinonStub;
+        const tokenA = "ZRX";
+        const tokenB = "ETH";
+
+        before(() => {
+            iocContainer.rebind(TYPES.TokenPairsService).to(TokensWithLiquidityTokenPairsService).inSingletonScope();
+        });
+        after(() => {
+            iocContainer.rebind(TYPES.TokenPairsService).to(TokensWithLiquidityTokenPairsService);
+        });
+
+        beforeEach(() => {
+            stub = sinon.stub(iocContainer.get<TokenPairsService>(TYPES.TokenPairsService), "listPairs").returns([]);
+        });
+        afterEach(() => {
+            stub.restore();
+        });
+
+        it("should call tokenPairsService.listPairs upon calling its listPairs", () => {
+            const controller = iocContainer.get<TokenPairsController>(TokenPairsController);
+            const returned = controller.listPairs();
+            // tslint:disable-next-line:no-unused-expression
+            expect(stub).to.be.calledOnce;
+        });
+        it("should pass tokenA as first argument to tokenPairsService.listPairs", () => {
+            const controller = iocContainer.get<TokenPairsController>(TokenPairsController);
+            const returned = controller.listPairs(tokenA);
+            expect(stub).to.be.calledWith(tokenA);
+        });
+        it("should pass tokenA as first argument and tokenB as second argument to tokenPairsService.listPairs", () => {
+            const controller = iocContainer.get<TokenPairsController>(TokenPairsController);
+            const returned = controller.listPairs(tokenA, tokenB);
+            expect(stub).to.be.calledWith(tokenA, tokenB);
+        });
     });
 });
