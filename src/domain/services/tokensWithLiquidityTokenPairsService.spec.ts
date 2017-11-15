02ac1c55a7635f7e2f62ec24bba4daeeb3faf9c4
Eduardo Lima <edulima@gmail.com>
2017-11-15 00:08:36 -0200

    test(TokensWithLiquidityTokenPairsService): should be unit tested.
    Including a complete unit test for TokensWithLiquidityTokenPairsService.

--- src/domain/services/tokensWithLiquidityTokenPairsService.spec.ts
+++ src/domain/services/tokensWithLiquidityTokenPairsService.spec.ts
@@ -1,6 +1,87 @@
+import { BigNumber } from "bignumber.js";
+import { expect, use } from "chai";
+import { Container, interfaces } from "inversify";
+import "reflect-metadata";
+import * as sinon from "sinon";
+import * as sinonChai from "sinon-chai";
+import { LiquidityService, TickerService, TokenPairsService, TokenService, TYPES } from "../../app";
+import { Ticker, Token, TokenPool } from "../../app/models";
+import { TokensWithLiquidityTokenPairsService } from "./tokensWithLiquidityTokenPairsService";
+
+use(sinonChai);
+
+const DEFAULT_ADDRESS = "0x0000000000000000000000000000000000000";
+const TOKENS = ["TK1", "TK2", "TK3"];
+
+function createToken(symbol: string): Token {
+    return {
+        address: DEFAULT_ADDRESS + symbol,
+        symbol,
+        decimals: 18,
+    };
+}
+
+const stubTokenService: TokenService = {
+    getToken: (symbol) => Promise.resolve(createToken(symbol)),
+    listAllTokens: () => Promise.resolve(TOKENS.map((symbol) => createToken(symbol))),
+};
+
+const stubLiquidityService: LiquidityService = {
+    getAvailableAmount: (token) => Promise.resolve({
+        token,
+        availableAmount: new BigNumber(10),
+        maximumAmount: new BigNumber(10),
+        minimumAmount: new BigNumber(0),
+        precision: 5,
+    }),
+};
+const stubTickerService: TickerService = {
+    getTicker: (tokenFrom, tokenTo) => Promise.resolve({
+        from: tokenFrom,
+        to: tokenTo,
+        bid: new BigNumber(1),
+        ask: new BigNumber(1.01),
+    }),
+};
+
+const iocContainer = new Container({ defaultScope: "Singleton" });
+iocContainer.bind<TokenPairsService>(TYPES.TokenPairsService).to(TokensWithLiquidityTokenPairsService);
+iocContainer.bind<LiquidityService>(TYPES.LiquidityService).toConstantValue(stubLiquidityService);
+iocContainer.bind<TickerService>(TYPES.TickerService).toConstantValue(stubTickerService);
+iocContainer.bind<TokenService>(TYPES.TokenService).toConstantValue(stubTokenService);
+
 describe("TokensWithLiquidityTokenPairsService", () => {
-    it.skip("when tokenA informed, returns pairs with tokenA as left-hand side or right-hand side of pair.");
-    it.skip("when tokenB informed, returns pairs with tokenB as left-hand side or right-hand side of pair.");
-    it.skip("when tokenA and tokenB informed, returns pairs with at least one of the tokens and the other token of the pair is tokenC.");
-    it.skip("when tokenA and tokenB informed, returns pairs with tokenA and tokenB as one pair (in either side of the pair).");
+    it("when tokenA informed, returns pairs with tokenA as left-hand side or right-hand side of pair.", async () => {
+        const symbol = TOKENS[1];
+        const address = DEFAULT_ADDRESS + symbol;
+        const returned = await iocContainer.get<TokenPairsService>(TYPES.TokenPairsService).listPairs(symbol);
+        // tslint:disable-next-line:no-unused-expression
+        expect(returned.find((pair) => pair.tokenA.address === address)).to.not.be.null;
+        // tslint:disable-next-line:no-unused-expression
+        expect(returned.find((pair) => pair.tokenB.address === address)).to.not.be.null;
+    });
+    it("when tokenB informed, returns pairs with tokenB as left-hand side or right-hand side of pair.", async () => {
+        const symbol = TOKENS[1];
+        const address = DEFAULT_ADDRESS + symbol;
+        const returned = await iocContainer.get<TokenPairsService>(TYPES.TokenPairsService).listPairs(null, symbol);
+        // tslint:disable-next-line:no-unused-expression
+        expect(returned.find((pair) => pair.tokenA.address === address)).to.not.be.null;
+        // tslint:disable-next-line:no-unused-expression
+        expect(returned.find((pair) => pair.tokenB.address === address)).to.not.be.null;
+    });
+    it("when tokenA and tokenB informed, returns pairs with at least one of the tokens and the other token of the pair is tokenC.", async () => {
+        const returned = await iocContainer.get<TokenPairsService>(TYPES.TokenPairsService).listPairs(TOKENS[0], TOKENS[1]);
+        // tslint:disable-next-line:no-unused-expression
+        expect(returned.find((pair) => pair.tokenB.address === DEFAULT_ADDRESS + TOKENS[2])).to.not.be.null;
+    });
+    it("when tokenA and tokenB informed, returns pairs with tokenA and tokenB as one of the pairs (in that order).", async () => {
+        const returned = await iocContainer.get<TokenPairsService>(TYPES.TokenPairsService).listPairs(TOKENS[0], TOKENS[1]);
+        // tslint:disable-next-line:no-unused-expression
+        expect(returned.find((pair) => pair.tokenA.address === DEFAULT_ADDRESS + TOKENS[0] && pair.tokenB.address === DEFAULT_ADDRESS + TOKENS[1])).to.not.be.null;
+    });
+    it("when tokenA and tokenB informed, returns pairs with tokenA and tokenB as one of the pairs (in reversed order).", async () => {
+        const returned = await iocContainer.get<TokenPairsService>(TYPES.TokenPairsService).listPairs(TOKENS[1], TOKENS[0]);
+        // tslint:disable-next-line:no-unused-expression
+        expect(returned.find((pair) => pair.tokenA.address === DEFAULT_ADDRESS + TOKENS[0] && pair.tokenB.address === DEFAULT_ADDRESS + TOKENS[1])).to.not.be.null;
+    });
 });
