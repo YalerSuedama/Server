17a6c0f7ac4b4b50acd3cf3dca94b7c16dc0b8a0
Eduardo Lima <edulima@gmail.com>
2017-12-30 23:09:25 -0200

    fix(TimeService): all times should be in utc

--- src/domain/services/timeServiceImpl.ts
+++ src/domain/services/timeServiceImpl.ts
@@ -8,6 +8,6 @@ export class TimeServiceImpl implements TimeService {
     private readonly expirationMilliseconds = 60000;
 
     public getExpirationTimestamp(): string {
-        return new BigNumber(moment().add(this.expirationMilliseconds, "ms").unix()).toString();
+        return new BigNumber(moment().utc().add(this.expirationMilliseconds, "ms").unix()).toString();
     }
 }
