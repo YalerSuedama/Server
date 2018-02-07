7af3f7e9238f21dd41c5638db09564d254dd0e95
Eduardo Lima <eduardo.lima@dtidigital.com.br>
2018-02-07 10:49:06 -0200

    test(FromCacheTickerService): FromCacheTickerService should be covered through unit testing

--- src/domain/services/ticker/fromCacheTickerService.ts
+++ src/domain/services/ticker/fromCacheTickerService.ts
@@ -8,18 +8,25 @@ import { Ticker, Token } from "../../../app/models";
 @injectable()
 export class FromCacheTickerService implements TickerService {
 
+    private static DEFAULT_TTL = moment.duration(5, "minutes");
     private static CachedTickers = new ExpirationStrategy(new MemoryStorage());
+
+    protected cacheItemTTL: moment.Duration;
 
     constructor( @inject(TYPES.LoggerService) private logger: LoggerService) {
         this.logger.setNamespace("tickercache");
+        if (config.has("amadeus.ticker.cache.ttl")) {
+            const ttlConfig: any = config.get("amadeus.ticker.cache.ttl");
+            this.cacheItemTTL = moment.duration(ttlConfig.value, ttlConfig.unit);
+        } else {
+            this.cacheItemTTL = FromCacheTickerService.DEFAULT_TTL;
+        }
     }
 
     public async addTicker(ticker: Ticker): Promise<void> {
         const tickerKey = this.getTickerKey(ticker.from, ticker.to);
-        const ttlConfig: any = config.get("amadeus.ticker.cache.ttl");
-        const ttl = moment.duration(ttlConfig.value, ttlConfig.unit);
-        this.logger.log("Adding ticker %s with price of %s to cache for %s", tickerKey, ticker.price.toString(), ttl.humanize());
-        await FromCacheTickerService.CachedTickers.setItem(tickerKey, ticker, { ttl: ttl.asSeconds() });
+        this.logger.log("Adding ticker %s with price of %s to cache for %s", tickerKey, ticker.price.toString(), this.cacheItemTTL.humanize());
+        await FromCacheTickerService.CachedTickers.setItem(tickerKey, ticker, { ttl: this.cacheItemTTL.asSeconds() });
     }
 
     public async getTicker(from: Token, to: Token): Promise<Ticker> {
@@ -32,6 +39,10 @@ export class FromCacheTickerService implements TickerService {
             this.logger.log("Did not found a ticker for %s", tickerKey);
         }
         return ticker;
+    }
+
+    public async clear(): Promise<void> {
+        await FromCacheTickerService.CachedTickers.clear();
     }
 
     private getTickerKey(from: Token, to: Token) {
